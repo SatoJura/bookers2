f53d88fb72b5bf280e31e180604fa4b83afff574
SatoJura <jnnsk0snb@gmail.com>
2021-08-07 21:24:32 +0000

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,34 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @user = current_user
+    
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render "index"
+    end
+
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @book.save
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
