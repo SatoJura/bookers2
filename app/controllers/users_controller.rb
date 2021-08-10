eb66a701abda85a9d130f36dcc09a9eb28ef2b12
SatoJura <jnnsk0snb@gmail.com>
2021-08-10 13:42:56 +0000

    bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,31 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    if @user.save
+      flash[:notice]="Welcome! You have signed up successfully."
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction,)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '130632bdc206102b60d13886fe10595c4535c00fa6d1fb564cec2df6478a8a6c431575c921fe37d8f82f023a469cb6b0f669456fba0b4cc1b720e2a13113c38c'
