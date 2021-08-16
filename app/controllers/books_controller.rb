67cc0b9935b9c219dd8f0b475b1eb0a324b619c1
SatoJura <jnnsk0snb@gmail.com>
2021-08-16 07:24:54 +0000

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book_new = Book.new
-    @user = current_user
+    # @user = current_user
   end
 
   def show
@@ -13,11 +13,11 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
       flash[:notice]="You have created book successfully."
-      redirect_to book_path(@book)
+      redirect_to book_path(@book_new)
     else
       @books = Book.all
       render "index"
